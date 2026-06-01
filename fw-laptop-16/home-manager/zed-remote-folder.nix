c0b517d653f4bbd61ddc63de7fc102bb771b1f86
qhorgues <quentin.horgues@outlook.fr>
2026-06-01 15:26:53 +0700

    add vn lab server

--- fw-laptop-16/home-manager/zed-remote-folder.nix
+++ fw-laptop-16/home-manager/zed-remote-folder.nix
@@ -3,6 +3,18 @@
     programs.zed-editor.userSettings = {
       ssh_connections = [
           {
+            host = "aiotlab.viu.edu.vn";
+            projects = [
+                {
+                    paths = [
+                        "~/PERSON_RLF"
+                    ];
+                }
+            ];
+            username = "quentin.horgues";
+            port = 30101;
+          }
+          {
             host = "rpi-horgues";
             projects = [
               {
