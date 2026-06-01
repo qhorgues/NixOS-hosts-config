452fca0e4d6382f57bf11741bdd5504d346ec7fe
qhorgues <quentin.horgues@outlook.fr>
2026-06-01 15:26:25 +0700

    remove old commented AMD fix

--- fw-laptop-16/configuration.nix
+++ fw-laptop-16/configuration.nix
@@ -128,23 +128,6 @@
             '';
     };
 
-    # Fix for AMD GPU crash
-    # nixpkgs.overlays = [
-    #   (self: super:
-    #     let
-    #       version = "20250808";
-    #     in
-    #     {
-    #     linux-firmware = super.linux-firmware.overrideAttrs (old: {
-    #       version = version;
-    #       src = super.fetchurl {
-    #         url = "https://cdn.kernel.org/pub/linux/kernel/firmware/linux-firmware-${version}.tar.xz";
-    #         sha256 = "sha256-wClVG0WhWSbJ16XfGgtUAEQGTxkVfFf8Edkf0Kreg38=";
-    #       };
-    #       patches = [];
-    #     });
-    #   })
-    # ];
 
     nix.settings = {
       extra-platforms = [ "aarch64-linux" ];
